3f40a71c5840c860573701639377990e2a5ad295
adamwilczek <adam.wilczek@hemmersbach.com>
2023-12-20 11:51:32 +0100

    changed wim to cim

--- AutomaticPatchingResultsAdam.ps1
+++ AutomaticPatchingResultsAdam.ps1
@@ -1,10 +1,10 @@
-$Server2k22 = "KB5032198"
-$Server2k19 = "KB5032196"
-$Server2k16 = "KB5032197"
+$Server2k22 = "KB5033118"
+$Server2k19 = "KB5033371"
+$Server2k16 = "KB5033373"
 
-ForEach ($server in (Get-Content "c:\scripts\AutomaticPatchingResults\checkservers.txt")) {
+ForEach ($server in (Get-Content "c:\scripts\AutomaticPatchingResultsAdam\checkservers.txt")) {
 
-$OSNAME = (Get-WmiObject Win32_OperatingSystem -ComputerName $server -ErrorAction SilentlyContinue).caption
+$OSNAME = (Get-CimInstance Win32_OperatingSystem -ComputerName $server -ErrorAction SilentlyContinue).caption
 
 
 if ($OSNAME -contains "Microsoft Windows Server 2022 Standard")
@@ -17,16 +17,16 @@ elseif ($OSNAME -contains "Microsoft Windows Server 2016 Standard")
     { $update = $Server2k16
     }
 
-$CheckifExists = Get-HotFix -ComputerName $server | Where-Object {$_.HotFixID -eq $update} | Select InstalledOn -ErrorAction SilentlyContinue
+$CheckifExists = Get-HotFix -ComputerName $server | Where-Object {$_.HotFixID -eq $update} | Select-Object InstalledOn -ErrorAction SilentlyContinue
 $RebootPending = Test-PendingReboot -ComputerName $server -SkipConfigurationManagerClientCheck | Select-Object -ExpandProperty IsRebootPending -ErrorAction SilentlyContinue 
 
 if (![string]::IsNullOrEmpty($CheckifExists))
     {
-        out-file -filepath "\\srvde5007\AgentsInstallationFiles\text1.txt" -InputObject "$server $update $CheckifExists Pending Reboot status $RebootPending " -Append
+        out-file -filepath "C:\scripts\AutomaticPatchingResultsAdam\results.txt" -InputObject "$server $update $CheckifExists Pending Reboot status $RebootPending " -Append
     }
 else
     {
-        out-file -filepath "\\srvde5007\AgentsInstallationFiles\text1.txt" -InputObject "Update $update not present for $server Pending Reboot status $RebootPending" -Append
+        out-file -filepath "C:\scripts\AutomaticPatchingResultsAdam\results.txt" -InputObject "Update $update not present for $server Pending Reboot status $RebootPending" -Append
     }
 
 }
